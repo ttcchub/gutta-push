36a015dd522e68524d2b1d714e46aa3689507a68
Nikita <nl_h@mail.ru>
2023-02-06 11:00:39 +0200

    123

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -76,6 +76,7 @@ const Cart = () => {
         <div className="product-container">
           {cartItems.length >= 1 && cartItems.map((item) => (
             <div className="product" key={item._id}>
+
               <img src={urlFor(item?.image[0])} className="cart-product-image" />
               <div className="item-desc">
                 <div className="flex top">
